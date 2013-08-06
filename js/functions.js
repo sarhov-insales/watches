4df802f3dd2b2eb6ef9f4d0e096283f60ea9cc0c
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-08-06 13:30:06 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
-	   // The  sub menu open close
+
+
+// The  sub menu open close
 
 
 if ($(document.body).width() < 1200) {
-	$('li.item ul').each(function(index) {
+$('li.item ul').each(function(index) {
 $(this).prev().addClass('hassub')});
 $('li.item a').after('<span></span>');
 $('li.item ul').css('display','none');
@@ -28,6 +30,31 @@ return false;
 });
 }
 
+
+// slider
+
+ $(function () {
+        $('.rs-slider').refineSlide({
+maxWidth              : 1200,      // Max slider width - should be set to image width
+transition            : 'random',  // String (default 'cubeV'): Transition type ('custom', random', 'cubeH', 'cubeV', 'fade', 'sliceH', 'sliceV', 'slideH', 'slideV', 'scale', 'blockScale', 'kaleidoscope', 'fan', 'blindH', 'blindV')
+fallback3d            : 'sliceV', // String (default 'sliceV'): Fallback for browsers that support transitions, but not 3d transforms (only used if primary transition makes use of 3d-transforms)
+customTransitions     : [],       // Arr (Custom transitions wrapper)
+perspective           : 1000,      // Perspective (used for 3d transforms)
+useThumbs             : false,     // Bool (default true): Navigation type thumbnails
+useArrows             :true,   // Bool (default false): Navigation type previous and next arrows
+thumbMargin           : 3,        // Int (default 3): Percentage width of thumb margin
+autoPlay              : true,    // Int (default false): Auto-cycle slider
+delay                 : 5000,       // Int (default 5000) Time between slides in ms
+transitionDuration    : 800,      // Int (default 800): Transition length in ms
+startSlide            : 0,        // Int (default 0): First slide
+keyNav                : true,     // Bool (default true): Use left/right arrow keys to switch slide
+captionWidth          : 50,       // Int (default 50): Percentage of slide taken by caption
+arrowTemplate         : '<div class="rs-arrows"><a href="#" data-icon="&#x61;" class="rs-prev"></a><a href="#" data-icon="&#x62;" class="rs-next"></a></div>', // String: The markup used for arrow controls (if arrows are used). Must use classes '.rs-next' & '.rs-prev'
+onInit                : function () {}, // Func: User-defined, fires with slider initialisation
+onChange              : function () {}, // Func: User-defined, fires with transition start
+afterChange           : function () {}  // Func: User-defined, fires after transition end
+        });
+    });
 
 
 
