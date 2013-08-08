1f47c3f4d4095f01234d6e30388207d75bd99651
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-08-08 14:28:36 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -131,6 +131,30 @@ visibleItems: 3
 });
 
 
+
+$("#items_carousel").flexisel({
+visibleItems: 6,
+animationSpeed: 1500,
+autoPlay: false,
+autoPlaySpeed: 2000,            
+pauseOnHover: true,
+enableResponsiveBreakpoints: true,
+responsiveBreakpoints: { 
+portrait: { 
+changePoint:480,
+visibleItems: 2
+}, 
+landscape: { 
+changePoint:640,
+visibleItems: 3
+},
+tablet: { 
+changePoint:768,
+visibleItems: 5
+}
+}
+});
+
 // quantity
 
 $(function(){
