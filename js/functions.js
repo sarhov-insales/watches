40e74e17492eb48515c036c0e449d451c15fc89d
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-08-12 02:02:41 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,23 @@
 
 jQuery(document).ready(function($) {
 
+
+// mobile navigation
+
+var opener = $('.drop-open')
+var drop = $('.drop');
+
+$(opener).click(function() {
+  $(this).toggleClass('drop-close').next(drop).slideToggle(600);
+  if($(this).hasClass('drop-close')){
+    $(this).attr('data-icon', '=');
+  }
+  else{
+    $(this).attr('data-icon', '>');
+  }
+});
+
+
 // list-grid
 
 $(function() {
@@ -62,32 +79,7 @@ $('.cart-inner').click(function() {
 
 // The  sub menu open close
 
-if ($(document.body).width() < 1200) {
-$('li.item ul').each(function(index) {
-$(this).prev().addClass('hassub')});
-$('li.item a').after('<span></span>');
-$('li.item ul').css('display','none');
-$('li.item ul.active').css('display','block');
-$('li.item ul').each(function(index) {
-$(this).prev().addClass('open-close').click(function() {
-if (
-$(this).next().css('display') == 'none') {
-$(this).next().slideDown(400, function () {
-$(this).prev().removeClass('collapsed').addClass('expanded');
-});
-}else {
-$(this).next().slideUp(400, function () {
-$(this).prev().removeClass('expanded').addClass('collapsed');
-$(this).find('ul').each(function() {
-$(this).hide().prev().removeClass('expanded').addClass('collapsed');
-});
-});
-}
-return false;
-});
-});
-}
-
+// 
 
 // slider
 
